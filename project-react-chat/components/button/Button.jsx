cc9ac08a6b9ce2a7157c59814807c7840503d3fe
Sanne Sofie <sannesofie@me.com>
2022-05-10 15:53:43 +0200

    fixed app

---
+++ project-react-chat/components/button/Button.jsx
@@ -0,0 +1,10 @@
+import './button.css'
+
+
+export const Button = (props) => {
+    return (
+        <div className="button">
+        <button onClick={props.handle}>{props.name}</button>
+        </div>
+    );
+    };
---
+++ project-react-chat/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import './header.css'
+
+
+export const Header = () => {
+    return (
+        <header>
+            <h1 class="header">
+            <span class="a">A</span>
+            <span class="w">w</span>
+            <span class="e">e</span>
+            <span class="s">s</span>
+            <span class="o">o</span>
+            <span class="m">m</span>
+            <span class="e">e</span>
+            <br></br>
+            <span class="c">C</span>
+            <span class="h">h</span>
+            <span class="a">a</span>
+            <span class="t">t</span>
+
+    </h1>
+        </header>
+        )
+}
+
